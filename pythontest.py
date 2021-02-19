cc6262a0416c712443ed69a4ca90c886120546a7
daviddpalmer <daviddpalmer@users.noreply.github.com>
2021-02-19 10:15:51 -0500

    Add files via upload

---
+++ pythontest.py
@@ -0,0 +1,4 @@
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+solution = number1 + number2
+print("The sum of your numbers is {}".format(solution))
